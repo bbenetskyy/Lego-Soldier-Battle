d737a32ca31571c9bc02c8d29f0a258816d95093
Bohdan Benetskyi <benetskyybogdan@gmail.com>
2019-03-28 13:59:27 +0100

    #2 Add Fake Data Provider

--- src/BrickWars.Core/Models/ArmyModel.cs
+++ src/BrickWars.Core/Models/ArmyModel.cs
@@ -46,11 +46,18 @@ namespace BrickWars.Core.Models
     public class WarriorModel:BaseModel
     {
         private WarriorType type;
+        private bool isAlive;
 
         public WarriorType Type
         {
             get =>type;
             set => SetProperty(ref type, value);
+        }
+
+        public bool IsAlive
+        {
+            get => isAlive;
+            set => SetProperty(ref isAlive, value);
         }
 
         public MvxObservableCollection<Equipment> Equipments { get; set; }
