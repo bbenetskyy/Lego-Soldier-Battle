eba3e39b55d9a0f32820e3b152b5818cb5ffc4f6
Bohdan Benetskyi <benetskyybogdan@gmail.com>
2019-03-31 20:31:59 +0200

    #2 Navigation to Selected Army

--- src/BrickWars.Core/ViewModels/Home/HomeViewModel.cs
+++ src/BrickWars.Core/ViewModels/Home/HomeViewModel.cs
@@ -4,13 +4,39 @@ using System.Text;
 using System.Threading.Tasks;
 using BrickWars.Core.FakeData;
 using BrickWars.Core.Models;
+using BrickWars.Core.ViewModels.Contacts;
+using MvvmCross.Navigation;
 using MvvmCross.ViewModels;
 
 namespace BrickWars.Core.ViewModels.Home
 {
     public class HomeViewModel : BaseViewModel
     {
+        private readonly IMvxNavigationService navigationService;
+        private ArmyModel selectedArmy;
+
         public MvxObservableCollection<ArmyModel> ArmyList { get; set; }
+
+        public ArmyModel SelectedArmy
+        {
+            get => selectedArmy;
+            set
+            {
+                SetProperty(ref selectedArmy, value);
+                if(selectedArmy != null)
+                    NavigateToArmyDetailis();
+            }
+        }
+
+        public HomeViewModel(IMvxNavigationService navigationService)
+        {
+            this.navigationService = navigationService;
+        }
+
+        private void NavigateToArmyDetailis()
+        {
+            navigationService.Navigate<ContactsViewModel>();
+        }
 
         public override Task Initialize()
         {
